d8352367d1f419a8f7819060a8348478c32e9745
Kunj <shahkunj42@gmail.com>
2022-06-14 10:42:29 -0400

    commit tree e074b13ec436b7b46be3d57b11e0c5e5469e04bb

--- src/index.js
+++ src/index.js
@@ -1,1 +1,48 @@
-// write your code here
+fetch('http://localhost:3000/ramens')
+.then(resp => resp.json())
+.then(ramen => ramen.forEach(item => {
+    let ramenImg = document.createElement('img')
+    let ramenMenu = document.querySelector('#ramen-menu')
+    ramenImg.src = item.image
+    ramenImg.setAttribute('id', `${item.id}`)
+    ramenImg.addEventListener('click', () => {
+        let focusedRamenImg = document.querySelector('.detail-image')
+        let focusedRamenName = document.querySelector('.name')
+        let focusedRamenRestaurant = document.querySelector('.restaurant')
+        let ramenRating = document.querySelector('#rating-display')
+        let ramenComment = document.querySelector('#comment-display')
+        focusedRamenImg.src = item.image
+        focusedRamenName.textContent = item.name
+        focusedRamenRestaurant.textContent = item.restaurant
+        ramenRating.textContent = item.rating
+        ramenComment.textContent = item.comment 
+    })
+    ramenMenu.appendChild(ramenImg)
+}))
+
+let form = document.querySelector('form')
+form.addEventListener('submit', (e) =>{
+    e.preventDefault()
+    let newRamenName = e.target.name.value
+    let newRamenRestaurant = e.target.restaurant.value
+    let newRamenImg = e.target.image.value
+    let newRamenRating = e.target.rating.value
+    let newRamenComment = e.target['new-comment'].value
+    let ramenImg = document.createElement('img')
+    let ramenMenu = document.querySelector('#ramen-menu')
+    ramenImg.src = newRamenImg
+    ramenImg.addEventListener('click', () => {
+        let focusedRamenImg = document.querySelector('.detail-image')
+        let focusedRamenName = document.querySelector('.name')
+        let focusedRamenRestaurant = document.querySelector('.restaurant')
+        let ramenRating = document.querySelector('#rating-display')
+        let ramenComment = document.querySelector('#comment-display')
+        focusedRamenImg.src = newRamenImg
+        focusedRamenName.textContent = newRamenName
+        focusedRamenRestaurant.textContent = newRamenRestaurant
+        ramenRating.textContent = newRamenRating
+        ramenComment.textContent = newRamenComment  
+})
+ramenMenu.appendChild(ramenImg)
+form.reset()
+})
